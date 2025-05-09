75c69523742662873401cb2bfa1b69a70e74ef28
Aline Lourenço <libalourenco@icloud.com>
2025-05-09 12:03:38 -0300

    Add files via upload

---
+++ front-aula-15-javascript/js/script.js
@@ -0,0 +1,22 @@
+/*
+const botaoMenu = document.querySelector("header nav button");
+
+botaoMenu.addEventListener("click", () => {
+    console.log ("Agora VITAMINADO!")
+});*/
+
+//Declarações
+
+//Var
+var nome = "José"; //string
+
+//Let
+let idade = 23; //tudo é number
+
+console.log(nome);
+console.log(idade);
+
+idade = nome;
+console.log(idade);
+
+
